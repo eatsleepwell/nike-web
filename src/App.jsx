c9d29e183926ac725b1ecf9bc31d124cde8639fa
rizkydarm <rzdwn.dev@gmail.com>
2023-12-11 13:38:33 +0700

    initial

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+const App = () => {
+  return (
+    <main className="relative">
+      Nav
+      <section className="
+        xl:padding-1 
+        wide:padding-r
+        padding-b
+      ">
+        Hero
+      </section>
+    </main>
+  )
+}
+
+export default App
