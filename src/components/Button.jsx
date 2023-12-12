d7410a4150ace5aa9355fdae96f79c5f3489c93e
rizkydarm <rzdwn.dev@gmail.com>
2023-12-12 15:04:01 +0700

    super-quality-section-finished??

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,17 +1,17 @@
-const Button = ({label, iconURL}) => {
+const Button = ({ label, iconURL }) => {
   return (
-    <button className="flex justify-center 
+    <button
+      className="flex justify-center 
         items-center gap-2 px-7 py-4 border 
         font-monstserrat text-lg leading-none
-        bg-coral-red rounded-full text-white border-coral-red">
-        {label}
-        <img 
-            src={iconURL} 
-            alt="arrow" 
-            className="ml-2 rounded-full w-5 h-5"
-        />
+        bg-coral-red rounded-full text-white border-coral-red"
+    >
+      {label}
+      {iconURL && (
+        <img src={iconURL} alt="arrow" className="ml-2 rounded-full w-5 h-5" />
+      )}
     </button>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
